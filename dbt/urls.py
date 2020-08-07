fdff866d93482568b9cd1b1a0d5b6c726b6d43ad
slehaff <samir@lehaff.dk>
2020-08-07 10:17:10 +0200

    sa

--- dbt/urls.py
+++ dbt/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
+
 urlpatterns = [
-    path('train/', include('train.urls')),
+    path('', include('train.urls')),
     path('admin/', admin.site.urls),
 ]
 
--- train/urls.py
+++ train/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
-
+from django.contrib import admin
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    
+    path('admin/', admin.site.urls),
+    path('train/', views.index),
 ]
+
